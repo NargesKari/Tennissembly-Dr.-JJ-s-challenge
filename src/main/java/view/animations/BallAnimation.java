5e3d5c27d5c5aca7b52399d44be117dceb12eb0a
NargesKari <nargeskari84@gmail.com>
2025-02-08 02:01:53 +0330

    last commit with no asm & c file ^~^

--- src/main/java/view/animations/BallAnimation.java
+++ src/main/java/view/animations/BallAnimation.java
@@ -21,10 +21,8 @@ public class BallAnimation extends Transition {
     private final DoubleProperty X = new SimpleDoubleProperty();
     private final DoubleProperty Y = new SimpleDoubleProperty();
     private final DoubleProperty radius;
-    private final ArrayList<DoubleProperty> trailsX = new ArrayList<>(Arrays.asList(new SimpleDoubleProperty(),
-            new SimpleDoubleProperty(), new SimpleDoubleProperty(), new SimpleDoubleProperty()));
-    private final ArrayList<DoubleProperty> trailsY = new ArrayList<>(Arrays.asList(new SimpleDoubleProperty(),
-            new SimpleDoubleProperty(), new SimpleDoubleProperty(), new SimpleDoubleProperty()));
+    private final ArrayList<DoubleProperty> trailsX = new ArrayList<>(Arrays.asList(new SimpleDoubleProperty(), new SimpleDoubleProperty(), new SimpleDoubleProperty(), new SimpleDoubleProperty()));
+    private final ArrayList<DoubleProperty> trailsY = new ArrayList<>(Arrays.asList(new SimpleDoubleProperty(), new SimpleDoubleProperty(), new SimpleDoubleProperty(), new SimpleDoubleProperty()));
     Racket racket1;
     Racket racket2;
     double cal;
@@ -82,24 +80,21 @@ public class BallAnimation extends Transition {
 
         System.out.println("-> " + movementMode + " a: " + a + ",b: " + b + ",c: " + c);
         if (ball.getRadius() == courtWidth * 0.03) {
-            if (ball.getBoundsInParent().intersects(racket1.getCollisionArea().getBoundsInParent()))
-                racketHit(racket1);
+            if (ball.getBoundsInParent().intersects(racket1.getCollisionArea().getBoundsInParent())) racketHit(racket1);
             else if (ball.getBoundsInParent().intersects(racket2.getCollisionArea().getBoundsInParent()))
                 racketHit(racket2);
         }
         if (!isBetween(x, courtX, courtX + courtWidth)) {
             speedX *= -1;
         }
-        if (ball.getCenterY() < 0)
-            controller.ballExited(true);
-        if (ball.getCenterY() > courtY + courtHeight + 10)
-            controller.ballExited(false);
+        if (ball.getCenterY() < 0) controller.ballExited(true);
+        if (ball.getCenterY() > courtY + courtHeight + 10) controller.ballExited(false);
         if (movementMode != -1 && speedY != 0) {
             switch (movementMode) {
                 case 1 -> //Parabola
                 {
                     if ((cal = calculateParabola(time, a, b, c)) > 0) {
-                        radius.set(makeInBound(cal,0.1,0.03 * courtWidth ));
+                        radius.set(makeInBound(cal, 0.1, 0.03 * courtWidth));
                     } else radius.set(courtWidth * 0.03);
                     time += speedY;
                     x += speedX;
@@ -118,7 +113,7 @@ public class BallAnimation extends Transition {
     }
 
     void racketHit(Racket racket) {
-        speedX = a * (ball.getCenterX() - racket.getCollisionArea().getCenterX()) / 14;
+        speedX = movementMode != 2 ? a * (ball.getCenterX() - racket.getCollisionArea().getCenterX()) / 14 : 0;
         int bPrime = movementMode == -1 ? b : 1;
         speedY = (ball.getCenterY() < racket.getCollisionArea().getCenterY()) ? -bPrime * SPEEDY : bPrime * SPEEDY;
     }
@@ -137,6 +132,7 @@ public class BallAnimation extends Transition {
             b = divRoundAwayFromZero(b, 3.0);
             c *= 5;
         } else if (i == 2) {
+            speedX = 0;
             a *= 5;
             b = divRoundAwayFromZero(b, 3.0) * 10;
             c = divRoundAwayFromZero(c, 3.0);
